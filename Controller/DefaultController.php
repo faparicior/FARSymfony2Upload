2db943ebb1cf7e2be7a8eaeea41b7bdb3fe47f6e
Fernando Aparicio <faparicior@gmail.com>
2015-12-28 17:53:31 +0100

    packagist

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -55,26 +55,4 @@ class DefaultController extends FOSRestController
 
         return new JsonResponse(array('files' => $response));
     }
-
-    /**
-     * @Route("/save/{id_session}")
-     * @Method("POST")
-     *
-     * @param string $id_session
-     *
-     * @return JsonResponse
-     */
-    public function saveAction($id_session)
-    {
-        $php_session = $this->get('session')->getId();
-        $FARUpload = $this->get('far_symfony2_upload_bundle.far_symfony2_upload_lib.service');
-
-        $files = $FARUpload->getListFilesLocal($php_session, $id_session);
-        $files = $FARUpload->setListFilesPathRemote($files, $id_session);
-
-        $files = $FARUpload->syncFilesLocalRemote($files, true);
-        $files = $FARUpload->deleteFilesLocal($files);
-
-        return new JsonResponse(array('files' => $files));
-    }
 }
